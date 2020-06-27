9ba32171cada7a5cfc52f2cdacf94c483de79691
Shivam <54373797+shivam0110@users.noreply.github.com>
2020-06-28 01:05:50 +0530

    Update snake.cpp

--- Snake-Game/snake.cpp
+++ Snake-Game/snake.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <conio.h>
+//sleep function
+#include <Windows.h>
 using namespace std;
 
 bool gameover;
-const int width = 50, height = 20;
+const int width = 30, height = 30;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int ntail = 0;
 enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
 
@@ -24,30 +28,37 @@ void draw(){
 
     cout<<"\t\tScore = "<<score<<"\n\n\n";
 
-    for(int i=0; i<width;i++){
-        cout<<"#";
+    for(int i=0; i<width+2;i++){
+        cout<<"=";
     }
     cout<<endl;
 
     for(int i=0; i<height;i++){
-        for(int j=0; j<width;j++){
-            if(j==0 || j== (width-1))
+        for(int j=0; j<=width;j++){
+            if(j==0 || j== (width))
                 cout<<"#";
             
             if(i==y && j==x)
                 cout<<"O";
             else if (i == fruitY && j == fruitX)
                 cout<<"F";
-            else
-            {
+            else{
+                bool print = false;
+                for(int k=0; k<ntail; k++){
+                    if(tailX[k] == j && tailY[k] == i){
+                        cout<<"o";
+                        print= true;
+                    }
+                }
+                if(!print)
                     cout<<" ";
             }
         }
         cout<<endl;
     }
 
-    for(int i=0; i<width;i++){
-        cout<<"#";
+    for(int i=0; i<width+2;i++){
+        cout<<"=";
     }
     cout<<endl;
 }
@@ -75,6 +86,21 @@ void input(){
 }
 
 void logic(){
+
+    int prevX = tailX[0], prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for(int i=1; i<ntail; i++){
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
     switch (dir){
 
         case LEFT:
@@ -92,14 +118,34 @@ void logic(){
         default:
             break;
     }
-    if(x > width-1 || x < 0 || y > height || y<0){
-        gameover = true;
+    
+    //not go through walls
+    if(x > width || x < 0 || y > height || y<0){
+        //gameover = true;
+    }
+    
+    //go through walls
+    if(x>width) 
+        x=0;
+    else if(x < 0)
+        x= width-1;
+
+    if(y>height) 
+        y=0;
+    else if(y < 0)
+        y= height-1;
+
+
+    for(int i=0; i<ntail; i++){
+        if(tailX[i]==x && tailY[i]==y)
+            gameover=true;
     }
     if(x == fruitX && y == fruitY){
         score += 10;
         //fruit on a new position    
         fruitX = rand() % width;
         fruitY = rand() % height;
+        ntail++;
     }
 }
 
@@ -109,10 +155,11 @@ int main(){
         draw();
         input();
         logic();
+        //Sleep(10);  //can be used to adjust difficulty level
     }
     system("cls");
-    cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t GAME OVER";
+    cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t GAME OVER\n\n\t\t\tScore = "<<score<<"\n\n\n\t\t\t>>Press Enter to Exit";
     cin.get();
-    
+        
     return 0;
 }
